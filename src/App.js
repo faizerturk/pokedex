9c72eecc77470c576699c452965de3e19220dc4a
faizerturk <faizeeeturk@gmail.com>
2021-11-08 08:24:28 +0300

    Change app component to a class component
    Made app component a class component, added a constructor to initializes state, and passed that into a child component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from 'react';
 import './App.css';
 import PokedexHeader from './components/PokedexHeader';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      name: "Faize"
+    }
+  }
+  render() {
+    return (
     <div className="App">
-      <PokedexHeader author="@faizerturk" name={"Faize"} />
+      <PokedexHeader author="@faizerturk" name={this.state.name} />
       {/* <PokedexHeader author="@faize" name={"faize"} />*/}
     </div> 
   );
 }
-
+}
 export default App; //app ksımını export ediyo burda 
 //index.js de import
+ 
